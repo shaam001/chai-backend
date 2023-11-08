3bc665fd25b6d35b48e90da1893ecb7c71a26bb1
shyam kumar <shyamkr210@gmail.com>
2023-11-09 00:56:28 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // require('dotenv').config({path: './env'})
+// to use import for dotenv we have to set config as done below and add "-r dotenv/config --experimental-json-modules" in dev scripts after "nodemon" in package.json so that we can use this experimental feature in future version it may not require
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -11,8 +12,10 @@ connectDB()
 
 
 
-
-
+/* 
+//not a professional approach
+// we can also write programs to connect database within the index.js but this may mess your index.js file so better create a seprate file to write program about database 
+*/
 /*
 import express from "express"
 const app = express()
