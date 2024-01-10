2291ba04ea4c2a6dca0f613783f525871b1743e0
shyam <shyamkr210@gmail.com>
2024-01-11 00:33:38 +0530

    fix logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -31,8 +31,9 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
+
 export default router
